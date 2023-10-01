472b2cbe59ba3d979bb55e9eea2cf7ac3467ce2f
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2023-10-01 19:40:32 +0530

    learning dropdown

--- projects/src/App.js
+++ projects/src/App.js
@@ -2,12 +2,14 @@
 // import MuiButton from './components/MuiButton';
 import MuiTextFiled from './components/MuiTextFiled';
 import './App.css'
+import MuiSelect from './components/MuiSelect';
 function App() {
   return (
     <div className="App">
       {/* <MuiTypography/> */}
       {/* <MuiButton/> */}
-      <MuiTextFiled/>
+      {/* <MuiTextFiled/> */}
+      <MuiSelect/>
     </div>
   );
 }
--- projects/src/components/MuiSelect.js
+++ projects/src/components/MuiSelect.js
@@ -1,10 +1,20 @@
-import React   from 'react'
-import { Box } from '@mui/material'
+import React, { useState }   from 'react'
+import { Box,TextField,MenuItem } from '@mui/material'
 
 const MuiSelect = () => {
+    const [country,setcountryies] = useState([])
+    const handleChange = (event) =>{
+        const value=event.target.value
+        setcountryies(typeof value === 'string'? value.split(',') : value)  
+    }
+    console.log(country)
   return (
     <Box width={"250px"}>
-      
+      <TextField label='select  country' select onChange={handleChange} fullWidth value={country} SelectProps={{multiple:true}} size="small" color='secondary' helperText='plesase select your country' error>
+        <MenuItem value="in">India</MenuItem>
+        <MenuItem value="ua">United America</MenuItem>
+        <MenuItem value="ch">China</MenuItem>
+      </TextField>
     </Box>
   )
 }
